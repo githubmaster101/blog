74ee7caaddf8760f7e2e6a55651bf667f0b41622
Kevin <yardman10550@gmail.com>
2020-02-02 12:46:42 -0500

    configure reducer to retrieve blog posts, display blog posts, create FETCH_USER action creator , create userHeader component

---
+++ src/components/UserHeader.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends Component {
+    componentDidMount() {
+        this.props.fetchUser(this.props.userId);
+    }
+    render() {
+        return (
+            <div>
+                User Header mon!
+            </div>
+        );
+    }
+}
+
+export default connect(null, { fetchUser })(UserHeader);
